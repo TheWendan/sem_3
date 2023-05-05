0e587c70d41a7d658f930792a040fd66fa0f9743
TheWendan <79003457+TheWendan@users.noreply.github.com>
2023-05-05 23:05:21 +0300

    Add files via upload

---
+++ 1/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "31.h"
+#define LEN 25
+
+int main()
+{
+    char s_inp[LEN] = {'0'};
+    unsigned int s_len;
+    printf("Confines: ");
+    scan(s_inp);
+    s_len = length(s_inp);
+    //if(s_inp[0] == '-')
+    //    putchar('-');
+    reverse(s_inp, s_len);
+    //if(s_inp[s_len -1] == '-')
+    //    putchar('-');  
+    return 0;
+}
